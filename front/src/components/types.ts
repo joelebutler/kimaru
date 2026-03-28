3e197bda94f1667394b30e35ad2778266eac168b
lmurray3-sudo <lmurray3@oakland.edu>
2026-03-28 17:42:33 -0400

    added more themes

--- front/src/components/types.ts
+++ front/src/components/types.ts
@@ -1,2 +1,6 @@
-export type Theme = "default" | "dark" | "sunset";
-export const Themes: Theme[] = ["default", "dark", "sunset"];
+export type Theme = "default" | "dark" | "sunset" | "peach" | "twilightsparkle" | 
+"DIO" | "teal" | "koi" | "gamer" | "sky" | "dusk" | "cherry" | "sophia" | "sunshine" | "brat" 
+| "pie" | "joel" | "sophia" | "mark" | "lilly" | "HEY";
+export const Themes: Theme[] = ["default","dark", "cherry" ,"peach","koi", "sunset", "DIO" ,
+     "teal" , "sky", "twilightsparkle" , "dusk" , "gamer", "sophia" , "sunshine" , "brat"
+    , "pie" , "joel" , "sophia" , "mark" , "lilly" , "HEY"];
