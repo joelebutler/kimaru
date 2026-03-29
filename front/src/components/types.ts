0c9152dbc58f863e82d7624243d8067d6c012d7c
lmurray3-sudo <lmurray3@oakland.edu>
2026-03-28 21:08:13 -0400

    commit

--- front/src/components/types.ts
+++ front/src/components/types.ts
@@ -7,4 +7,4 @@ export type Theme = "default" | "dark" | "sunset" | "peach" | "twilightsparkle" 
 | "pie" | "joel" | "sophia" | "mark" | "lilly" | "HEY";
 export const Themes: Theme[] = ["default","dark", "cherry" ,"peach","koi", "sunset", "DIO" ,
      "teal" , "sky", "twilightsparkle" , "dusk" , "gamer", "sophia" , "sunshine" , "brat"
-    , "pie" , "joel" , "sophia" , "mark" , "lilly" , "HEY"];
+    , "pie" , "joel" , "sophia" , "mark" , "lilly" , "HEY"]; 
